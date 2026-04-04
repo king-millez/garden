53216fb2877ee08db78a534fb69c19beddaab570
king-millez <millez.dev@gmail.com>
2026-04-04 12:44:46 +1100

    Inject mouse position in render method

--- simulation/src/lib.rs
+++ simulation/src/lib.rs
@@ -1,17 +1,16 @@
 use std::sync::Arc;
-
+use wasm_bindgen::{prelude::*, JsCast};
 use web_sys::{CanvasRenderingContext2d, HtmlCanvasElement};
 use winit::{
   application::ApplicationHandler,
   event::*,
   event_loop::{ActiveEventLoop, EventLoop},
   keyboard::{KeyCode, PhysicalKey},
+  platform::web::{EventLoopExtWebSys, WindowAttributesExtWebSys},
   window::Window,
 };
 
-use wasm_bindgen::prelude::*;
-#[cfg(target_arch = "wasm32")]
-use winit::platform::web::EventLoopExtWebSys;
+const CANVAS_ID: &str = "canvas";
 
 pub struct State {
   window: Arc<Window>,
@@ -42,36 +41,44 @@ impl State {
     self.canvas.set_height(height);
   }
 
-  pub fn render(&mut self) {
-    let width = self.canvas.width() as f64;
-    let height = self.canvas.height() as f64;
-    let cx = width / 2.0;
-    let cy = height / 2.0;
-
-    self.ctx.set_fill_style_str("rgba(0, 0, 0, 0.07)");
-    self.ctx.fill_rect(0.0, 0.0, width, height);
-
-    let amplitude = height * 0.3;
-    let frequency = 0.02;
-    let speed = 0.05;
-
-    self.ctx.begin_path();
-    self.ctx.set_stroke_style_str("rgba(0, 255, 180, 0.9)");
-    self.ctx.set_line_width(2.0);
-
-    let steps = width as i32;
+  fn draw_curve(&mut self, steps: i32, cx: f64, cy: f64, amplitude: f64, frequency: f64) {
     for i in 0..=steps {
       let x = i as f64;
       let phase = self.t + (x - cx) * frequency;
       let y = cy + amplitude * phase.sin();
 
-      if i == 0 {
-        self.ctx.move_to(x, y);
-      } else {
+      if i > 0 {
+        let prev_x = ((i - 1) as f64 - 0.8) as f64;
+        let prev_phase = self.t + (prev_x - cx) * frequency;
+        let prev_y = cy + amplitude * prev_phase.sin();
+
+        self.ctx.begin_path();
+        self.ctx.set_stroke_style_str("rgb(243, 158, 1)");
+        self.ctx.move_to(prev_x, prev_y);
         self.ctx.line_to(x, y);
+        self.ctx.stroke();
       }
     }
-    self.ctx.stroke();
+  }
+
+  pub fn render(&mut self, (mouse_x, mouse_y): (f64, f64)) {
+    let width = self.canvas.width() as f64;
+    let height = self.canvas.height() as f64;
+    let cx = width / 2.0;
+    let cy = height / 2.0;
+
+    self.ctx.set_fill_style_str("rgb(0, 0, 0)");
+    self.ctx.fill_rect(0.0, 0.0, width, height);
+
+    let amplitude = height * 0.2 * (1.0 - ((mouse_y / height) * 2.0).abs());
+    let frequency = 0.02 * (1.0 - ((mouse_x / width) * 2.0).abs());
+    let speed = 0.03;
+
+    self.ctx.begin_path();
+    self.ctx.set_line_width(2.0);
+
+    let steps = width as i32;
+    self.draw_curve(steps, cx, cy, amplitude, frequency);
 
     self.t += speed;
     self.window.request_redraw();
@@ -79,24 +86,23 @@ impl State {
 }
 
 pub struct App {
-  proxy: Option<winit::event_loop::EventLoopProxy<State>>,
+  proxy: winit::event_loop::EventLoopProxy<State>,
   state: Option<State>,
+  cursor_pos: (f64, f64),
 }
 
 impl App {
   pub fn new(event_loop: &EventLoop<State>) -> Self {
-    let proxy = Some(event_loop.create_proxy());
-    Self { state: None, proxy }
+    Self {
+      state: None,
+      proxy: event_loop.create_proxy(),
+      cursor_pos: (0.0, 0.0),
+    }
   }
 }
 
 impl ApplicationHandler<State> for App {
   fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-    use wasm_bindgen::JsCast;
-    use winit::platform::web::WindowAttributesExtWebSys;
-
-    const CANVAS_ID: &str = "canvas";
-
     let window = wgpu::web_sys::window().unwrap_throw();
     let document = window.document().unwrap_throw();
     let canvas = document.get_element_by_id(CANVAS_ID).unwrap_throw();
@@ -106,26 +112,26 @@ impl ApplicationHandler<State> for App {
     let window_attributes = Window::default_attributes().with_canvas(Some(html_canvas_element));
     let window = Arc::new(event_loop.create_window(window_attributes).unwrap());
 
-    if let Some(proxy) = self.proxy.take() {
-      wasm_bindgen_futures::spawn_local(async move {
-        assert!(proxy
-          .send_event(
-            State::new(window, canvas_element)
-              .await
-              .expect("Unable to create state!")
-          )
-          .is_ok())
-      });
-    }
+    let proxy = self.proxy.clone();
+
+    wasm_bindgen_futures::spawn_local(async move {
+      assert!(proxy
+        .send_event(
+          State::new(window, canvas_element)
+            .await
+            .expect("Unable to create state.")
+        )
+        .is_ok())
+    });
   }
 
-  #[allow(unused_mut)]
   fn user_event(&mut self, _event_loop: &ActiveEventLoop, mut event: State) {
     event.window.request_redraw();
     event.resize(
       event.window.inner_size().width,
       event.window.inner_size().height,
     );
+
     self.state = Some(event);
   }
 
@@ -141,10 +147,13 @@ impl ApplicationHandler<State> for App {
     };
 
     match event {
+      WindowEvent::CursorMoved { position, .. } => {
+        self.cursor_pos = (position.x, position.y);
+      }
       WindowEvent::CloseRequested => event_loop.exit(),
       WindowEvent::Resized(size) => state.resize(size.width, size.height),
       WindowEvent::RedrawRequested => {
-        state.render();
+        state.render(self.cursor_pos);
       }
       WindowEvent::KeyboardInput {
         event:
